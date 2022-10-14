d4ba6eb0bbe1a96049f15897938731550ca82655
sebahi <sebahi.omar@gmail.com>
2022-10-14 16:12:06 -0400

    Added print message

--- service-b/src/main/java/com/example/sns/serviceb/controller/Receiver.java
+++ service-b/src/main/java/com/example/sns/serviceb/controller/Receiver.java
@@ -24,10 +24,8 @@ public class Receiver {
         public void receiveNotification(@NotificationMessage String message, 
           @NotificationSubject String subject) {
             // handle message
-<<<<<<< HEAD
             System.out.println(message);
-=======
->>>>>>> 633ce10527d3d8149aa298c2612afbbdd8500caf
+
         }
 
         @NotificationUnsubscribeConfirmationMapping
