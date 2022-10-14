633ce10527d3d8149aa298c2612afbbdd8500caf
sebahi <sebahi.omar@gmail.com>
2022-10-14 16:05:56 -0400

    Added message dependancy and defined receive controller

---
+++ service-b/src/main/java/com/example/sns/serviceb/controller/Receiver.java
@@ -0,0 +1,35 @@
+package com.example.sns.serviceb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.awspring.cloud.messaging.config.annotation.NotificationMessage;
+import io.awspring.cloud.messaging.config.annotation.NotificationSubject;
+import io.awspring.cloud.messaging.endpoint.NotificationStatus;
+import io.awspring.cloud.messaging.endpoint.annotation.NotificationMessageMapping;
+import io.awspring.cloud.messaging.endpoint.annotation.NotificationSubscriptionMapping;
+import io.awspring.cloud.messaging.endpoint.annotation.NotificationUnsubscribeConfirmationMapping;
+
+@Controller
+@RequestMapping("/receive")
+public class Receiver {
+
+        @NotificationSubscriptionMapping
+        public void confirmUnsubscribeMessage(
+          NotificationStatus notificationStatus) {
+            notificationStatus.confirmSubscription();
+        }
+     
+        @NotificationMessageMapping
+        public void receiveNotification(@NotificationMessage String message, 
+          @NotificationSubject String subject) {
+            // handle message
+        }
+
+        @NotificationUnsubscribeConfirmationMapping
+        public void confirmSubscriptionMessage(
+          NotificationStatus notificationStatus) {
+            notificationStatus.confirmSubscription();
+        }
+
+}
